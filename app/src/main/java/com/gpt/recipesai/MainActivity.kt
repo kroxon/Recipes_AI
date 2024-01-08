28616ece17e909c74ebafca0faf30ea929cfde0d
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-01-08 18:14:40 +0100

    commit

--- app/src/main/java/com/gpt/recipesai/MainActivity.kt
+++ app/src/main/java/com/gpt/recipesai/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -30,9 +31,13 @@ import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -49,11 +54,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gpt.recipesai.data.models.Message
@@ -191,11 +198,121 @@ fun MainUI(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SegmentedControl(
+    modifier: Modifier = Modifier,
+    items: List<String>,
+    defaultSelectedItemIndex: Int = 0,
+    useFixedWidth: Boolean = false,
+    itemWidth: Dp = 120.dp,
+    cornerRadius: Int = 24,
+    onItemSelection: (selectedItemIndex: Int) -> Unit = {}
+) {
+    val selectedIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
+    val itemIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(38.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = if (selectedIndex.value == itemIndex.value) {
+
+                MaterialTheme.colorScheme.background
+            } else {
+
+                MaterialTheme.colorScheme.secondary
+            }
+        ),
+        shape = RoundedCornerShape(cornerRadius)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.secondary),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            items.forEachIndexed { index, item ->
+                itemIndex.value = index
+                Card(
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(2.dp),
+                    onClick = {
+                        selectedIndex.value = index
+                        onItemSelection(selectedIndex.value)
+                    },
+                    colors = CardDefaults.cardColors(
+                        containerColor = if (selectedIndex.value == index) {
+                            MaterialTheme.colorScheme.background
+                        } else {
+                            MaterialTheme.colorScheme.secondary
+                        },
+                        contentColor = if (selectedIndex.value == index)
+                            MaterialTheme.colorScheme.scrim
+                        else
+                            MaterialTheme.colorScheme.onSecondary
+                    ),
+                    shape = when (index) {
+                        0 -> RoundedCornerShape(
+                            topStartPercent = cornerRadius,
+                            topEndPercent = cornerRadius,
+                            bottomStartPercent = cornerRadius,
+                            bottomEndPercent = cornerRadius
+                        )
+
+                        items.size - 1 -> RoundedCornerShape(
+                            topStartPercent = cornerRadius,
+                            topEndPercent = cornerRadius,
+                            bottomStartPercent = cornerRadius,
+                            bottomEndPercent = cornerRadius
+                        )
+
+                        else -> RoundedCornerShape(
+                            topStartPercent = 0,
+                            topEndPercent = 0,
+                            bottomStartPercent = 0,
+                            bottomEndPercent = 0
+                        )
+                    },
+                ) {
+                    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                        Text(
+                            text = item,
+                            style = LocalTextStyle.current.copy(
+                                fontSize = 14.sp,
+                                fontWeight = if (selectedIndex.value == index)
+                                    LocalTextStyle.current.fontWeight
+                                else
+                                    FontWeight.Normal,
+                                color = if (selectedIndex.value == index)
+                                    MaterialTheme.colorScheme.scrim
+                                else
+                                    MaterialTheme.colorScheme.onSecondary
+                            ),
+                            textAlign = TextAlign.Center,
+
+                            )
+                    }
+                }
+            }
+        }
+    }
 
 }
+
+//@Preview
+//@Composable
+//fun MainUIPreview() {
+//    MainUI(messages = emptyList())
+//}
 
 @Preview
 @Composable
-fun MainUIPreview() {
-    MainUI(messages = emptyList())
+fun SegmentPreview() {
+    SegmentedControl(items = listOf("Śniadanie", "Obiad", "Kolacja", "Lunch", "Deser", "Dowolne"))
 }
+
