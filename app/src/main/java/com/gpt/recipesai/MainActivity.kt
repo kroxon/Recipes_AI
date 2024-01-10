08b29cc00bc0122387d116ae0719855ca12fd66a
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-01-10 13:55:06 +0100

    commit

--- app/src/main/java/com/gpt/recipesai/MainActivity.kt
+++ app/src/main/java/com/gpt/recipesai/MainActivity.kt
@@ -4,46 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -54,11 +37,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -69,6 +48,7 @@ import com.gpt.recipesai.data.network.GptCommunicator
 import com.gpt.recipesai.ui.theme.RecipesAITheme
 import com.gpt.recipesai.ui.theme.color1
 import com.gpt.recipesai.ui.theme.color2
+import com.gpt.recipesai.ui.theme.color6
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -82,9 +62,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp),
-                    color = MaterialTheme.colorScheme.background
+                        .fillMaxSize(),
+                    color = color6
                 ) {
                     val prompt = "Przepis na dziś: \n"
 //                    var gptResponse by remember {
@@ -97,15 +76,37 @@ class MainActivity : ComponentActivity() {
                         mutableStateOf(false)
                     }
 
+                    var selectedMealIndex by remember { mutableStateOf(5) }
+                    val mealsList = stringArrayResource(id = R.array.meals_array).toList()
+
+                    var excludedProducts by remember {
+                        mutableStateOf("")
+                    }
+
+                    var mealCharacteristics by remember {
+                        mutableStateOf("")
+                    }
+
                     MainUI(
                         messages = messages,
                         loading = loading,
-                        onButtonClick = {
+                        defaultMealIndex = selectedMealIndex,
+                        meals = mealsList,
+                        onGenerateClick = {
                             loading = true
                             scope.launch {
                                 val result = gpt.fetchGptResponse(prompt)
                                 loading = false
                             }
+                        },
+                        onMealSelection = {
+                            selectedMealIndex = it
+                        },
+                        onExcludedInsert = {
+
+                        },
+                        onFeaturesInserted = {
+
                         }
                     )
 
@@ -145,13 +146,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainUI(
     messages: List<Message>,
+    meals: List<String>,
+    defaultMealIndex: Int,
     loading: Boolean = false,
-    onButtonClick: () -> Unit = {}
+    onGenerateClick: () -> Unit = {},
+    onMealSelection: (selectedMealIndex: Int) -> Unit,
+    onExcludedInsert: (String) -> Unit,
+    onFeaturesInserted: (String) -> Unit
 ) {
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Blue)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color6)
+            .padding(8.dp)
+    ) {
 //        Row(
 //            modifier = Modifier
 //                .fillMaxWidth(),
@@ -178,7 +187,14 @@ fun MainUI(
 //            }
 //        }
 
-        SegmentedControl()
+        SegmentedControl(
+            defaultSelectedItemIndex = defaultMealIndex,
+            items = meals,
+            onMealSelection = onMealSelection
+        )
+
+        ExcludedProduct(onExcludedInsert = onExcludedInsert)
+        MealsFeatures(onFeaturesInserted = onFeaturesInserted)
 
         if (loading) {
             Row(
@@ -210,11 +226,11 @@ fun MainUI(
 fun SegmentedControl(
     modifier: Modifier = Modifier,
     items: List<String> = stringArrayResource(id = R.array.meals_array).toList(),
-    defaultSelectedItemIndex: Int = 0,
+    defaultSelectedItemIndex: Int,
     useFixedWidth: Boolean = false,
     itemWidth: Dp = 120.dp,
     cornerRadius: Int = 24,
-    onItemSelection: (selectedItemIndex: Int) -> Unit = {}
+    onMealSelection: (selectedMealIndex: Int) -> Unit
 ) {
     val selectedIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
     val itemIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
@@ -248,11 +264,11 @@ fun SegmentedControl(
                         .padding(2.dp),
                     onClick = {
                         selectedIndex.value = index
-                        onItemSelection(selectedIndex.value)
+                        onMealSelection(selectedIndex.value)
                     },
                     colors = CardDefaults.cardColors(
                         containerColor = if (selectedIndex.value == index) {
-                            color2
+                            color6
                         } else {
                             color1
                         },
@@ -264,15 +280,15 @@ fun SegmentedControl(
                     shape = when (index) {
                         0 -> RoundedCornerShape(
                             topStartPercent = cornerRadius,
-                            topEndPercent = cornerRadius,
+                            topEndPercent = 0,
                             bottomStartPercent = cornerRadius,
-                            bottomEndPercent = cornerRadius
+                            bottomEndPercent = 0
                         )
 
                         items.size - 1 -> RoundedCornerShape(
-                            topStartPercent = cornerRadius,
+                            topStartPercent = 0,
                             topEndPercent = cornerRadius,
-                            bottomStartPercent = cornerRadius,
+                            bottomStartPercent = 0,
                             bottomEndPercent = cornerRadius
                         )
 
@@ -284,11 +300,11 @@ fun SegmentedControl(
                         )
                     },
                 ) {
-                    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                         Text(
                             text = item,
                             style = LocalTextStyle.current.copy(
-                                fontSize = 14.sp,
+                                fontSize = 12.sp,
                                 fontWeight = if (selectedIndex.value == index)
                                     LocalTextStyle.current.fontWeight
                                 else
@@ -309,15 +325,60 @@ fun SegmentedControl(
 
 }
 
+@Composable
+fun ExcludedProduct(
+    onExcludedInsert: (String) -> Unit
+
+) {
+    var input by remember { mutableStateOf("") }
+    Row {
+        TextField(
+            value = input,
+            onValueChange = { onExcludedInsert(it) },
+            shape = ShapeDefaults.Medium,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+}
+
+@Composable
+fun MealsFeatures(
+    onFeaturesInserted: (String) -> Unit
+) {
+    var input by remember { mutableStateOf("") }
+    Row {
+        TextField(
+            value = input,
+            onValueChange = { onFeaturesInserted(it) },
+            shape = ShapeDefaults.Medium,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+}
+
 //@Preview
 //@Composable
 //fun MainUIPreview() {
 //    MainUI(messages = emptyList())
 //}
 
-@Preview
-@Composable
-fun SegmentPreview() {
-    SegmentedControl()
-}
+//@Preview
+//@Composable
+//fun SegmentPreview() {
+//    SegmentedControl(
+//        defaultSelectedItemIndex = 3,
+//        onMealSelection = {}
+//    )
+//}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ExcludedProductPreview() {
+    ExcludedProduct(onExcludedInsert = {})
+}
